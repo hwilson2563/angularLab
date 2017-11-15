cd66d9c2f87c1d5b9c34e649741852f221c60ab4
Hilary Claire <hilary.wilson2563@gmail.com>
2017-11-14 21:16:47 -0500

    part one of angular lab submit

---
+++ javascript/controller.js
@@ -0,0 +1,17 @@
+
+(function(){
+	function FormController(){
+		var vm = this;
+		vm.itemList = [ ];
+		vm.addItem = function(adding){
+			vm.itemList.push(adding);
+			vm.add = "";
+			}
+		vm.removeItem = function(index){
+			vm.itemList.splice(index, 1);
+		}
+	};
+	angular
+		.module("app")
+		.controller("FormController",FormController);
+})();
